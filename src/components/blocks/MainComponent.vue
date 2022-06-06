a729b817b9c1ee0db798a2be455e83d27ef5eaf3
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-06-06 18:26:08 +0200

    aggiunta sezione serie, con utilizzzo delle props

--- src/components/blocks/MainComponent.vue
+++ src/components/blocks/MainComponent.vue
@@ -1,16 +1,20 @@
 <template>
     <main>
         <HeroSection/>
+        <SeriesSection/>
     </main>
 </template>
 
 <script>
 import HeroSection from '../sections/HeroSection.vue'
+import SeriesSection from '../sections/SeriesSection.vue'
 export default {
     name: 'MainComponent',
     components: {
-        HeroSection
-    }
+        HeroSection,
+        SeriesSection,
+    },
+   
 }
 </script>
 
---
+++ src/components/commons/CardSeries.vue
@@ -0,0 +1,35 @@
+<template>
+        <div class="card">
+            <img :src="thumb" :alt="title">
+            <div class="title">
+                <small>{{series}}</small>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'CardSeries',
+    props: {
+        thumb: String,
+        series: String
+    }
+}
+</script>
+
+<style  lang="scss" scoped>
+  img{
+        height: 200px;
+        width: 200px;
+        object-fit: cover;
+    }
+
+    .title{
+        color:white; 
+        margin: 10px 0;
+    }
+
+    .card{
+    width: 100%
+}
+</style>
